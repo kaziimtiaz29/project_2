2790997f8ac875d83d134d143ee959db819778d6
kaziimtiaz29 <uddinimtiaz29@yahoo.com>
2021-06-10 13:43:50 +0000

    edit test.sh

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -4,8 +4,8 @@ sudo apt-get update
 #sudo apt install chromium-chromedriver -y
 #sudo apt install python3-venv
 
-sudo apt-get install python3 python3-pip python3-venv -y 
-python3 -m venv venv
+sudo apt-get install python3-pip python3-venv -y 
+#python3 -m venv venv
 source venv/bin/activate
 pip3 install -r test_requirements.txt
 
