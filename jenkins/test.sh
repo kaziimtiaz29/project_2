14fa51ee12b1d9c32aa05c383136ebdce65c2c8a
kaziimtiaz29 <uddinimtiaz29@yahoo.com>
2021-06-10 13:35:07 +0000

    edit test.sh

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -7,7 +7,7 @@ sudo apt-get update
 sudo apt-get install python3 python3-pip python3-venv -y 
 python3 -m venv venv
 source venv/bin/activate
-pip3 -r install test_requirements.txt
+pip3 install -r test_requirements.txt
 
 #pytest coverage server
 cd server
