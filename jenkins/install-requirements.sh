a7be7ecda1c04158f7ae01abb33bd69ae393f024
kaziimtiaz29 <uddinimtiaz29@yahoo.com>
2021-06-11 09:00:09 +0000

    edit

--- jenkins/install-requirements.sh
+++ jenkins/install-requirements.sh
@@ -23,7 +23,10 @@ sudo chmod +x /usr/local/bin/docker-compose
 docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD
 
 #ansible
-sudo apt update 
-sudo apt install software-properties-common
-sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible -y
+mkdir -p ~/.local/bin
+echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
+source ~/.bashrc
+
+sudo apt install python3-pip -y
+
+pip3 install --user ansible
