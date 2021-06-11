67482c6bd69c9b8a0782f878f7665e8621fe025a
kaziimtiaz29 <uddinimtiaz29@yahoo.com>
2021-06-11 11:43:46 +0000

    asdqw

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-scp docker-compose.yaml swarm-manager:
-ssh swarm-manager << EOF 
-export DATABASE_URI=${DATABASE_URI}
-docker stack deploy --compose-file docker-compose.yaml project
+
+# copy over compose yaml to manager node
+scp -i ~/.ssh/ansible_id_rsa docker-compose.yaml jenkins@manager:/home/jenkins/docker-compose.yaml
+
+# docker stack deploy
+ssh -i ~/.ssh/ansible_id_rsa jenkins@manager << EOF
+    export DATABASE_URI=${DATABASE_URI}
+    docker stack deploy --compose-file docker-compose.yaml lucky_prize
 EOF
